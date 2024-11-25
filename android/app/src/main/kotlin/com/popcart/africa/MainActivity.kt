b0c58fea1729ace758766afbf9d1de0c8216dc45
Adedola Owen Abaru <56795990+Software78@users.noreply.github.com>
2024-11-25 12:28:33 +0100

    project files generated

---
+++ android/app/src/main/kotlin/com/popcart/africa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.popcart.africa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:popcart/app/app.dart';
+import 'package:popcart/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
