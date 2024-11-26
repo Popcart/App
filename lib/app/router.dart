8ce3f1b503f186e24f8eecbe489daf0481eec8b8
Adedola Owen Abaru <56795990+Software78@users.noreply.github.com>
2024-11-26 13:20:21 +0100

    chore: update dependencies and adjust iOS platform version

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,9 +1,6 @@
-import 'dart:async';
-
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:popcart/app/router_paths.dart';
 import 'package:popcart/features/onboarding/screens/video_splash_screen.dart';
@@ -11,14 +8,15 @@ import 'package:popcart/features/onboarding/screens/video_splash_screen.dart';
 final rootNavigatorKey = GlobalKey<NavigatorState>();
 
 final router = GoRouter(
-    debugLogDiagnostics: kDebugMode,
-    navigatorKey: rootNavigatorKey,
-    observers: [
-      FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
-    ],
-    routes: [
-      GoRoute(
-        path: AppPath.splash.path,
-        builder: (context, state) => const VideoSplashScreen(),
-      ),
-    ]);
+  debugLogDiagnostics: kDebugMode,
+  navigatorKey: rootNavigatorKey,
+  observers: [
+    FirebaseAnalyticsObserver(analytics: FirebaseAnalytics.instance),
+  ],
+  routes: [
+    GoRoute(
+      path: AppPath.splash.path,
+      builder: (context, state) => const VideoSplashScreen(),
+    ),
+  ],
+);
